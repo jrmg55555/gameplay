bc32d99abf2c8f7cacc52e435189202b0215c585
Jean Mendes <jeanrodrigo30@gmail.com>
2021-06-22 06:53:51 -0400

    gameplay

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Text, View, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IllustrationImg from "../../assets/illustration.png";
+import { styles } from "./style";
+
+export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate("Home");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {"\n"} e organize suas {"\n"}
+          jogatinas
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos pra jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon
+          title="Entrar com Discord"
+          activeOpacity={0.7}
+          onPress={handleSignIn}
+        />
+      </View>
+    </View>
+  );
+}
